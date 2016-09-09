5435b1cbfdb2f3f313e34e3106981ad69074901a
rossukon963 <rossukon.sril57@sskru.ac.th>
2016-09-09 14:25:08 +0700

    new project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my Name is Rossukon Srilachai 5711423126");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("press any key to exit.");
+           Console.ReadKey();     
+
+        }
+    }
+}
